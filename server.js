4b5fb97a2a35d862ed1cf6b17d3129ed051c7051
SalihZinaty <salihzinaty@gmail.com>
2020-01-07 14:21:35 +0200

    fix

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ const knex = require('knex');
 const db = knex({
     client:'pg',
     connection:{
-        connectionString: process.env.DATABASE_URL,
+        connectionString : process.env.DATABASE_URL,
         ssl: true
     }
 });
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
+
 // we need to separate the tables of DB
 /* CREATE TABLE users ( 
     id serial PRIMARY KEY, 
